1b9d115f4b05bd3dcbbfbdb854d951b152fb0e77
Jose Soncco (LMint) <josesoal@gmail.com>
2020-04-20 19:55:47 -0300

    Updates

---
+++ precache-manifest.546e631a0228a0c7dd55d566a8f5804b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72147d189f859dc445a282071355063",
+    "url": "/react-animation-canvas/index.html"
+  },
+  {
+    "revision": "2b5e10046838ed112c16",
+    "url": "/react-animation-canvas/static/css/main.8719c3e0.chunk.css"
+  },
+  {
+    "revision": "71aa91f7ba170ab60da6",
+    "url": "/react-animation-canvas/static/js/2.41cbbd9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-animation-canvas/static/js/2.41cbbd9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5e10046838ed112c16",
+    "url": "/react-animation-canvas/static/js/main.9142e731.chunk.js"
+  },
+  {
+    "revision": "593a5db82a5b6d4104e9",
+    "url": "/react-animation-canvas/static/js/runtime-main.27498e54.js"
+  }
+]);
